17fd7140809bdb88e91b40e45fb909c8198f5733
th0mk <th0mk@users.noreply.github.com>
2021-02-26 14:57:08 +0100

    Update analytics.js

--- docs/javascript/analytics.js
+++ docs/javascript/analytics.js
@@ -1,9 +1,9 @@
 app.location$.subscribe(function (url) {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
-      dataLayer.push(arguments);
+        dataLayer.push(arguments);
     }
     gtag("js", new Date());
-  
+
     gtag("config", "G-2NVJXMTT2N");
-  });
+}); 
